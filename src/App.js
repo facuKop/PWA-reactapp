afa27c1a2942f3368c00570941dbfdcda766b607
facuKop <facundo.kopech@southworks.com>
2023-05-02 16:25:31 -0300

    adding PWA service worker and web manifest

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello this is a React web App
+          Hello this is a React web App!!!!
         </p>
         <a
           className="App-link"
